61c1a4749483f67c6eacfd6c4e269e55cf2e48b2
Maythem <Maythem20@gmail.com>
2020-07-24 10:58:47 -0400

    Initial Commit

---
+++ iExpenses/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iExpenses
+//
+//  Created by Maythem Alsodani on 7/24/20.
+//  Copyright © 2020 Maythem Alsodani. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
